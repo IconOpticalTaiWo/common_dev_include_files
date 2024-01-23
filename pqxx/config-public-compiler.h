47c57be287d668d82d61597a911a3138b81959e4
IconOpticalTaiWo <135949943+IconOpticalTaiWo@users.noreply.github.com>
2024-01-23 12:07:59 +0800

    .

---
+++ pqxx/config-public-compiler.h
@@ -0,0 +1,11 @@
+/* Automatically generated from config.h: public/compiler config. */
+
+#define PQXX_HAVE_CMP 1
+#define PQXX_HAVE_CONCEPTS 1
+#define PQXX_HAVE_GCC_PURE 1
+#define PQXX_HAVE_GCC_VISIBILITY 1
+#define PQXX_HAVE_LIKELY 1
+#define PQXX_HAVE_PATH 1
+#define PQXX_HAVE_SPAN 1
+#define PQXX_HAVE_STRERROR_R 1
+#define PQXX_HAVE_YEAR_MONTH_DAY 1
